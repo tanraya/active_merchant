a4dfa10c95455ec15ba55e7939e88ebd8323fdc5
tanraya <demerest@gmail.com>
2014-11-29 19:38:53 +0500

    Remove unused stuff

--- test/unit/integrations/kiwi_module_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class QiwiModuleTest < Test::Unit::TestCase
-  include ActiveMerchant::Billing::Integrations
-
-  def test_notification_method
-    assert_instance_of Qiwi::Notification, Qiwi.notification('name=cody')
-  end
-end
